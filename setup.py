3570e2e530b285a204bb3020d57c6b8c867f0d36
harsh <harshnivande749@gmail.com>
2024-07-20 21:58:10 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-import os
+from setuptools import find_packages, setup
+
+setup(
+    name='Information Retriever System',
+    version='0.0.0',
+    author='Harsh',
+    author_email='harshnivande749@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
